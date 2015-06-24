4c3a9b5ed399a04b507a4c24c7b4d4f4768702a5
tvolpes <timothybvolpe@gmail.com>
2015-06-24 16:12:25 -0400

    Started render functionality

--- MCMapper2/rendererbasic.cpp
+++ MCMapper2/rendererbasic.cpp
@@ -1,11 +1,23 @@
 #include "renderer.h"
 #include "rendererbasic.h"
+#include "nbt.h"
 
 CRendererBasic::CRendererBasic() {
 
 }
 
-bool CRendererBasic::render() {
+bool CRendererBasic::render()
+{
+	PathList regionPaths;
 
+	// Find region files
+	regionPaths = this->getRegionFiles();
+
+	// Loop through each region file
+	for( PathList::iterator it = regionPaths.begin(); it != regionPaths.end(); it++ )
+	{
+
+	}
+	
 	return true;
 }
