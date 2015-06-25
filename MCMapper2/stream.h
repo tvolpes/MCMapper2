0e97857861cd7587591b935caf3309fcaf2174ea
tvolpes <timothybvolpe@gmail.com>
2015-06-25 10:41:41 -0400

    Centralized stream stuff

---
+++ MCMapper2/stream.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <boost\filesystem.hpp>
+#include <boost\filesystem\fstream.hpp>
+#include <boost\iostreams\filtering_stream.hpp>
+
+typedef boost::iostreams::filtering_istream InputStream;
