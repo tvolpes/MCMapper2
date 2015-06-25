5d48b8b7add5c7ee249a8a1e34e8ce6a04ff1e23
tvolpes <timothybvolpe@gmail.com>
2015-06-25 10:47:08 -0400

    Added gzip

--- MCMapper2/stream.h
+++ MCMapper2/stream.h
@@ -3,5 +3,8 @@
 #include <boost\filesystem.hpp>
 #include <boost\filesystem\fstream.hpp>
 #include <boost\iostreams\filtering_stream.hpp>
+#pragma warning( disable:4244 )
+#include <boost\iostreams\filter\gzip.hpp>
+#pragma warning( default:4244 )
 
 typedef boost::iostreams::filtering_istream InputStream;
