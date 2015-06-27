b7c56e42ea35cb1f4abea21879c2e719bbbfec55
tvolpes <timothybvolpe@gmail.com>
2015-06-27 16:26:32 -0400

    Work on image rendering

--- MCMapper2/rendererbasic.cpp
+++ MCMapper2/rendererbasic.cpp
@@ -18,6 +18,10 @@
 
 */
 
+#pragma warning( disable:4996 )
+#include <boost\gil\gil_all.hpp>
+#include <boost\gil\extension\io\png_dynamic_io.hpp>
+#pragma warning( default:4996 )
 #include <iostream>
 #include <boost\endian\conversion.hpp>
 #include "def.h"
@@ -34,8 +38,10 @@ bool CRendererBasic::render()
 {
 	PathList regionPaths;
 	boost::filesystem::ifstream inStream;
+	boost::filesystem::path regionImagePath;
 	RegionHeader *pRegionHeader;
 	CChunk *pCurrentChunk;
+	boost::gil::rgb8_image_t *pRegionImage;
 
 	// Find region files
 	regionPaths = this->getRegionFiles();
@@ -60,25 +66,59 @@ bool CRendererBasic::render()
 			if( !pRegionHeader )
 				return false;
 
+			// Create the image
+			pRegionImage = new boost::gil::rgb8_image_t( 512, 512 );
+
 			// Loop through all the chunks
 			for( unsigned int i = 0; i < REGION_CHUNKCOUNT; i++ )
 			{
-				// Progress percentage
-				//std::cout << "\r\t > Parsing chunk... " << ((double)(i+1)/(double)REGION_CHUNKCOUNT)*100.0 << "%              " << std::flush;
+				boost::gil::rgb8_image_t::view_t imageView;
+				boost::int32_t xPos, zPos, xRelPos, zRelPos;
 
 				// If theres no data, skip it
 				if( pRegionHeader->size[i] == 0 )
 					continue;
 				// Read the chunk
-				pCurrentChunk = this->readChunk( inStream );
+				pCurrentChunk = this->readChunk( inStream, CHUNKLOADFLAGS_USE_POS | CHUNKLOADFLAGS_USE_HEIGHTMAP );
 				// If there's not chunk here continue
 				if( !pCurrentChunk )
 					continue;
 				
-				// Do stuff. . .
+				// Get the data to work with
+				imageView = boost::gil::view( *pRegionImage );
+
+				// Get position and relative position
+				xPos = pCurrentChunk->getXPos()->payload();
+				zPos = pCurrentChunk->getZPos()->payload();
+				xRelPos = abs( xPos % 32 );
+				zRelPos = abs( zPos % 32 );
+
+				// . . .
+				for( int32_t x = xRelPos*16; x < xRelPos*16+16; x++ )
+				{
+					for( int32_t z = zRelPos*16; z < zRelPos*16+16; z++ )
+					{
+						imageView( x, z ) = boost::gil::rgb8_pixel_t( 0, 205, 205 );
+					}
+				}
+				
+				// Clean up
+				if( pCurrentChunk ) {
+					delete pCurrentChunk;
+					pCurrentChunk = NULL;
+				}
 			}
+
+			// Write the image file
+			regionImagePath = m_outputPath / (*it).stem();
+			regionImagePath += ".png";
+			boost::gil::png_write_view( regionImagePath.string(), boost::gil::const_view( *pRegionImage ) );
 		
 			// Clean up
+			if( pRegionImage ) {
+				delete pRegionImage;
+				pRegionImage = NULL;
+			}
 			if( pRegionHeader ) {
 				delete pRegionHeader;
 				pRegionHeader = NULL;
